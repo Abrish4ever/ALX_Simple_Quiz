3a853ae7acb9e438866c0c489cffa13062cee7b4
Abrish <abrish4ever2015@gmail.com>
2024-07-13 16:09:39 +0300

    add strict equal

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer() {
     var correctAnswer= "4";
     var userAnswer= document.querySelector('input[name="quiz"]:checked').value;
-    if(userAnswer==correctAnswer) {
+    if(userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent= "Correct! Well done.";
     } else {
         document.getElementById('feedback').textContent= "That's incorrect. Try again!";
