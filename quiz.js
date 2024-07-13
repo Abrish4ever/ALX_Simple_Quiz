105af9e2c215368c8bc5bae8fb0ebbf2bbabf5ca
Abrish <abrish4ever2015@gmail.com>
2024-07-13 16:19:45 +0300

    add strict inequality

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     var userAnswer= document.querySelector('input[name="quiz"]:checked').value;
     if(userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent= "Correct! Well done.";
-    } else {
+    } else if(userAnswer !== correctAnswer) {
         document.getElementById('feedback').textContent= "That's incorrect. Try again!";
     }
 }
